91656f578f6ece175857651a0d420311071b4162
lvsc <lvscxl@sina.com>
2021-09-07 16:31:57 +0800

    asd

--- build/build.js
+++ build/build.js
@@ -5,13 +5,16 @@ process.env.NODE_ENV = 'production'
 
 const ora = require('ora')
 const rm = require('rimraf')
+
 const path = require('path')
 const chalk = require('chalk')
 const webpack = require('webpack')
 const config = require('../config')
+
 const webpackConfig = require('./webpack.prod.conf')
 
 const spinner = ora('building for production...')
+
 spinner.start()
 
 rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
